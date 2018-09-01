ae3ca7a71874d7994739634ca49898808618de38
wcy <wcy1231@qq.com>
2018-09-01 15:35:48 +0800

    change color

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,6 +1,6 @@
 ace.require("ace/ext/language_tools");
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/solarized_dark");
 editor.getSession().setMode("ace/mode/c_cpp");
 
 //editor.container.style.background= rgb(15,15,15);
