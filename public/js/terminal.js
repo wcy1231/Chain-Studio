e875360e9d67bb4977b51444148faa148bef2df9
tygavin <tianyao@zju.edu.cn>
2018-09-02 13:09:55 +0800

    textarea div

--- public/js/terminal.js
+++ public/js/terminal.js
@@ -5,7 +5,20 @@ var term = new Vue({
   },
   methods: {
     writeln(t){
-      this.text += t + '\n'
+      var textdiv = document.getElementById("terminal_text");
+      var reg = /(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-)+)/g;
+      var url = t.match(reg);
+      console.log(url);
+      if (url!=null){
+        textdiv.innerHTML += '<a href="'+t+'">'+t+'</a><br>';
+      }
+      else{
+        textdiv.innerHTML += t + '<br>';
+      }
+      textdiv.scrollTop = textdiv.scrollHeight;
+
+
+      //this.text += t + '\n'
     }
   }
 })
